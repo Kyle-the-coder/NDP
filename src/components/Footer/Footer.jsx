73d8a0ef873df79aa53eb8d842c9f813af9b5348
Kyle-the-coder <kylethacoder@gmail.com>
2025-07-16 19:01:46 -0600

    updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,20 @@
 import logo from "../../assets/logo/NBDLogo.png";
-import contentDecor from "../../assets/decor/lines/SpaceLineDecorTB.png";
+import techOutline from "../../assets/decor/lines/techOutline.svg";
 import fb from "../../assets/icons/functIcons/facebook-logo.png";
 import ig from "../../assets/icons/functIcons/instagram.png";
 import email from "../../assets/icons/functIcons/envelope.png";
 import "./footer.css";
 import { Button } from "../Button/Button";
+import { useEffect, useState } from "react";
 
 export function Footer() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <section id="footer" className="footer-main">
       <div className="logo-container">
@@ -16,27 +24,31 @@ export function Footer() {
 
       <div className="footer-content-container">
         <div className="footer-content">
-          <div className="content-info">
-            <h1 className="bebas-thin-font letter-space">Class Info:</h1>
-            <div className="content-button">
-              <Button fontSize="3rem" text="Sign Up Here" />
-            </div>
+          <h1 className="bebas-thin-font letter-space">Class Info:</h1>
+          <div className="content-container center">
+            <Button
+              fontSize={windowWidth <= 900 ? "2rem" : "3rem"}
+              text="Sign Up Here"
+            />
           </div>
-          <img src={contentDecor} className="footer-content-decor" />
+
+          <img src={techOutline} className="footer-content-decor" />
         </div>
+
         <div className="footer-content">
-          <div className="content-info">
-            <h1 className="bebas-thin-font letter-space">Contact:</h1>
-            <div className="content-icon-container">
-              <img src={fb} className="contact-icon" />
-              <img src={ig} className="contact-icon" />
-              <img src={email} className="contact-icon" />
-            </div>
+          <h1
+            className="bebas-thin-font letter-space"
+            style={{ marginBottom: windowWidth <= 900 && "10px" }}
+          >
+            Contact:
+          </h1>
+          <div className="content-container">
+            <img src={fb} className="contact-icon" />
+            <img src={ig} className="contact-icon" />
+            <img src={email} className="contact-icon" />
           </div>
-          <img
-            src={contentDecor}
-            className="footer-content-decor-right footer-roto"
-          />
+
+          <img src={techOutline} className="footer-content-decor" />
         </div>
       </div>
     </section>
--- src/pages/LP/Lp.jsx
+++ src/pages/LP/Lp.jsx
@@ -2,7 +2,6 @@ import { Footer } from "../../components/Footer/Footer";
 import { Bio } from "../../sections/LandingSections/Bio/Bio";
 import { Hero } from "../../sections/LandingSections/Hero/Hero";
 import { Styles } from "../../sections/LandingSections/Styles/Styles";
-import "./lp.css";
 
 export default function Lp() {
   return (
