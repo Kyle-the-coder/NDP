431ca15c10b4f0dc5725daf534a563d240e7d0ae
Kyle-the-coder <kylethacoder@gmail.com>
2025-07-28 16:07:22 -0600

    updated

---
+++ src/sections/AboutSections/AboutInfo/AboutInfoTitle/AboutInfoTitle.jsx
@@ -0,0 +1,9 @@
+import "./aboutinfotitle.css";
+
+export function AboutInfoTitle() {
+  return (
+    <section className="about-info-title-main">
+      <h1 className="bebas-font letter-space">About Nebula Dance Program</h1>
+    </section>
+  );
+}
